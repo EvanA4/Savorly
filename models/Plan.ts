44e94fb2650340ed68020607afaf470af59e72a1
amy-huang16 <ahuang16@vols.utk.edu>
2026-02-25 14:22:05 -0500

    add plan type and model

---
+++ models/Plan.ts
@@ -0,0 +1,33 @@
+import { Plan } from "@/types/plan";
+import { Model, Schema, model, models, Document } from "mongoose";
+
+const PlanSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  creatorId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  sharedUsers: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  restaurants: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Restaurant",
+    },
+  ],
+});
+
+export type PlanDocument = Plan & Document;
+const PlanModel =
+  (models.Plan as Model<PlanDocument>) ||
+  model<PlanDocument>("Plan", PlanSchema);
+
+export default PlanModel;
---
+++ types/plan.ts
@@ -0,0 +1,6 @@
+export interface Plan {
+  name: string;
+  creatorId: string;
+  sharedUsers?: string[];
+  restaurants?: string[];
+}
