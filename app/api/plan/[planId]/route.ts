09d73cd9d8b88ad3d57e2781b3db8bba014d0b4c
amy-huang16 <ahuang16@vols.utk.edu>
2026-02-25 20:56:51 -0500

    add plan DELETE and GET

--- app/api/plan/[planId]/route.ts
+++ app/api/plan/[planId]/route.ts
@@ -1,2 +1,42 @@
+import PlanModel from "@/models/Plan";
+
+import dbConnect from "@/utils/dbconnect";
+import { NextRequest, NextResponse } from "next/server";
+
 // DELETE (delete plan)
+export const DELETE = async function (
+  req: NextRequest,
+  { params }: { params: { planId: string } },
+) {
+  await dbConnect();
+
+  if (!params.planId)
+    return NextResponse.json({ message: "Plan ID required" }, { status: 400 });
+
+  const plan = await PlanModel.findOneAndDelete({
+    _id: params.planId,
+  });
+
+  if (!plan)
+    return NextResponse.json({ message: "Plan not found" }, { status: 404 });
+
+  return NextResponse.json(plan, { status: 200 });
+};
+
 // GET (get plan)
+export const GET = async function (
+  req: NextRequest,
+  { params }: { params: { planId: string } },
+) {
+  await dbConnect();
+
+  if (!params.planId)
+    return NextResponse.json({ message: "Plan ID required" }, { status: 400 });
+
+  const plan = await PlanModel.find({ _id: params.planId });
+
+  if (!plan)
+    return NextResponse.json({ message: "Plan not found" }, { status: 404 });
+
+  return NextResponse.json(plan, { status: 200 });
+};
